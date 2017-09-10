64e65136e3b6bc5687356c98fed5a9d0fbc114be
jtblog <jtob91@yahoo.com>
2017-09-10 11:40:03 +0100

    Update

---
+++ aspire_files/pages/js/about.js
@@ -0,0 +1,88 @@
+
+function asastudent() {
+
+    var content = "<p> An opportunity to meet and share ideas with other young people, to gain insight into" +
+	"global innovation trend, understand emerging skills, gain and enhance insight you need to" +
+	"innovate, impact the world and have a fulfilling career, Come learn how to design and" +
+	"transform your ideas to products and meet other innovators, who are impacting their" +
+	"world and create a future for themselves.Join thousands of other young people to understand how scalable innovation happens, gain the" +
+	"skills, connections, and network you need to have an impactful future! </p>" +
+
+	"<h2> Reasons to join us: </h2>" +
+
+	"<ul style='background-color: transparent; list-style-type:square;'>" +
+			  "<li>Don’t be left behind, the world is not your father’s backyard, It is changing faster than you think</li> <br>" +
+			  "<li>Get Inspired to Aspire</li> <br>" +
+			  "<li>Network with accomplished speakers</li> <br>" +
+			  "<li>Get sharpened to identify needs and create disruptively</li> <br>" +
+			  "<li>Learn skills marketable in the global world, as an entrepreneur or a professional</li> <br>" +
+			  "<li>Learn from people leading in the innovation space. Be inspired</li> <br>" +
+			  "<li>Career opportunities</li> <br>" +
+			  "<li>Join a network of innovators and entrepreneur and have easier access to resources and funding</li>" +
+	"</ul><br><br>";
+
+	$("#right").html(content);
+}
+
+function asaprofessional() {
+
+    var content = "<p> ASPIRE 2016 offers a unique opportunity for professionals and entrepreneurs to gain" +
+	"insight on how to maximize impact in an increasingly complex world. Through our various" +
+	"speakers and planned events, they have an opportunity to learn the art of disruptive" +
+	"innovation, finding finances for your innovation and building a value oriented institution. <p><br>" +
+	"<p> An opportunity to network with other young professionals and acquire skills and" +
+	"knowledge to make the most impact in your field. Get to know, where the cheese is moved" +
+	"to and gain insight to lead others all the way. </p> <br>" +
+	"<p> From having a bite at where the world is heading, to understanding exponential innovation" +
+	"and the economics of innovation: leading within a global environment to leading changes," +
+	"planning within scenarios and managing projects, you also get to have a bite at managing" +
+	"co-creation , open innovation and leading a value-oriented firm. It is your chance to stand" +
+	"out! </p> <br>" +
+	"<p> Trust me, your skills are become obsolete. Get to see where the worls is heading and get ahead of" +
+	"it. Remain marketable in a global world and gain the consciousness of an innovator. Come" +
+	"network. Work with other professionals and let us give you experiences and skills to help you be the most impactful at work and in the society. Come sharpen your mind and learn skills to" +
+	"ideate, brainstorm and solve problems.</p>" +
+
+	"<h2> Reasons to join us: </h2>" +
+
+	"<ul style='background-color: transparent; list-style-type:square;'>" +
+			  "<li>Get Inspired to Aspire</li> <br>" +
+			  "<li>Network with accomplished speakers and successful innovators</li> <br>" +
+			  "<li>Learn skills to excel in the global world</li> <br>" +
+			  "<li>Learn to grow, be sustainable and impactful in your field</li> <br>" +
+			  "<li>See where to find the finances for your idea/innovation</li> <br>" +
+			  "<li>Career and partnership opportunities</li> <br>" +
+			  "<li>Become a leader in innovation management</li> <br>" +
+	"</ul><br><br>";
+
+	$("#right").html(content);
+}
+
+function asapservant() {
+    
+}
+function asaexhibitor() {
+	/*
+    By Registering as an exhibitor at ASPIRE 2016 EXPO, you will be associated with an initiative
+	and other organizations interested in youth, innovation, leadership development, technology and
+	social impact. This is the cheapest it can get to interactively get your products and services to an
+	informed and huge number of people within a short period. Be counted with the transformation
+	generation. Don’t be left behind, the a match us in.
+	Reasons to join us
+	• Hire high flyers
+	• Market your brand to more than 2000 young students and professionals
+	• Network with other in the innovation, entrepreneurship, technology, youth development
+	space
+	• Be associated with a conference with social impact goals*/
+}
+
+function asaspeaker() {
+    /*ASPIRE 2015 provides an opportunity for speakers to inspire a group of young students,
+	professionals and entrepreneur- our future leaders with their story. You could speak on an
+	interactive session, a panel or simply make presentation. When you speak at our event you also
+	an opportunity to:
+	• Connect and engage students, entrepreneurs and professionals at events; panel discussions,
+	networking theaters, and reception.
+	• Share your story, ideas, grow your network
+	• Let others see your firm’s work at our popular Expo.*/
+}
